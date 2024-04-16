22e9e8ac2f58a22dec579e51bfd79099e36e8169
Heizal <patriciaheizal@gmail.com>
2024-04-16 14:21:21 +0200

    set up database and server

---
+++ server/server.js
@@ -0,0 +1,21 @@
+// Initialise the port to run the server
+const PORT = process.env.PORT ?? 8000
+const express = require('express')
+const app = express()
+const pool = require('./db')
+
+// Get all todos from the  database
+app.get('/todos', async (req, res) =>{
+    const userEmail = 'heizal@test.com'
+    try{
+        // Get all todos  that match the users email
+        const todos = await pool.query('SELECT * FROM todos WHERE user_email = $1', [userEmail])
+        res.json(todos.rows)
+ 
+    } catch (err){
+        console.error(err)
+    }   
+})
+
+
+app.listen(PORT, ()=> console.log(`Server running on PORT ${PORT}`))
