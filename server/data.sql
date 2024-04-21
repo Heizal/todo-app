fc26a17316c29aad8f9e301c0d52fc8f5e8cad3a
Heizal <patriciaheizal@gmail.com>
2024-04-21 13:14:45 +0200

    Add sharedtodo table to database

--- server/data.sql
+++ server/data.sql
@@ -13,3 +13,10 @@ CREATE TABLE users(
     email VARCHAR(255) PRIMARY KEY,
     hashed_password VARCHAR(255)
 );
+
+CREATE TABLE sharedtodos (
+    user_email VARCHAR(255),
+    todo_id VARCHAR(255),
+    FOREIGN KEY (user_email) REFERENCES todos (user_email),
+    FOREIGN KEY (todo_id) REFERENCES todos(id)
+);
